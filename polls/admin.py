e6fb93604c400f193c04d94c7d101ba34a71c11b
Meyniel Matthieu <matthieu.meyniel@gfi.fr>
2018-08-21 14:23:20 +0200

    Personnaliser interface d'admin + static fils

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoicesTable(admin.TabularInline):
+    """
+    Permet d'afficher et de créer les choix depuis le
+    formulaire des questions
+    """
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Paramétrage de l'affichage des champs dans
+    l'interface d'administration Django
+    """
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoicesTable]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # ordering = ('-pub_date',)
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
